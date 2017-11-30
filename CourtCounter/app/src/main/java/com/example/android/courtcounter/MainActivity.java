866c616c725b255b435b81c452e18d3b18776db5
Ata Mahmoud <ata.ata250@hotmail.com>
2017-11-30 21:30:26 +0200

    Reset Button

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -65,4 +65,12 @@ public class MainActivity extends AppCompatActivity
         scoreTeamB+=1;
         displayForTeamB(scoreTeamB);
     }
+
+    public void reset(View view)
+    {
+        scoreTeamB=0;
+        scoreTeamA=0;
+        displayForTeamB(scoreTeamB);
+        displayForTeamA(scoreTeamA);
+    }
 }
